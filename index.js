846a7ed1894ff9c4f2091a1196040a428f4a60d2
mirzaistiakhislam <mirza15-9603@diu.edu.bd>
2022-12-16 22:42:07 +0600

    data insert done.

--- index.js
+++ index.js
@@ -10,16 +10,23 @@ const port = process.env.PORT || 5000;
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://preciouscameras:<password>@cluster0.b6byypn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.b6byypn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function fun() {
     try {
         const applicationCollections = client.db('LoanApplication').collection('applicationList');
+
         app.get('/', (req, res) => {
             res.send(`Server running: ${port}`);
         })
+        app.post('/addapplication',async (req, res) => {
+            const data  = req.body
+            const result = await applicationCollections.insertOne(data);
+            res.send(result);
+        })
+
     }
     finally {
     }
